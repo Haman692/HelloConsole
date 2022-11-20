aa0a63ca84690cc79b75c2d382c6cf71df555d31
Haman692 <haman692@yandex.ru>
2022-11-20 12:41:46 +0700

    IfElse

---
+++ example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя ");
+string Username = Console.ReadLine();
+if(Username.ToLower() == "данила")
+{
+    Console.Write("Приветствую, ");
+    Console.WriteLine(Username);
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(Username);
+}
