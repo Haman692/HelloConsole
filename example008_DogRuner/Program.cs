321e2ddcc48a14aefe6c68db0de4fa70a784b621
Haman692 <haman692@yandex.ru>
2022-11-20 14:37:27 +0700

    Example_DogRunner

---
+++ example008_DogRuner/Program.cs
@@ -0,0 +1,23 @@
+﻿double distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriensSpeed = 2;
+int dogSpeed = 5;
+int Friend = 2;
+int count = 0;
+double time;
+while(distance > 10)
+{
+    if(Friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        Friend = 2;
+    }
+    else
+    {
+        time = distance / (secondFriensSpeed + dogSpeed);
+        Friend = 1;
+    }
+    distance = distance - (firstFriendSpeed+secondFriensSpeed)*time;
+    count++;
+}
+Console.WriteLine(count); 
