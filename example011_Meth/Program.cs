0d5e6605dfc3a224b7c0bbeba07f4ca93b87b4e6
Haman692 <Haman692@yandex.ru>
2022-11-23 00:01:20 +0700

    Find

---
+++ example011_Meth/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 1, 12, 3, 422, 82, 51, 6, 37, 82 };
+
+int n = array.Length;
+int find = 82;
+int index = 0;
+while(index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
